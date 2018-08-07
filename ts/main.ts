d0d36591460b7121cdddd1801cffeb5cc0af7085
akosnad <akos.nadudvari@pinkcloud.hu>
2018-08-07 09:37:21 +0200

    Offloaded player logic to its own class

--- ts/main.ts
+++ ts/main.ts
@@ -1,8 +1,7 @@
 var ctx: any = null;
 
-var hero = new LivingSprite("/img/hero.png");
+var hero = new PlayerSprite("/img/hero.png");
 
-var keysDown: boolean[] = new Array<boolean>();
 var lastUpdate = Date.now();
 
 let initialize = function () {
@@ -10,19 +9,12 @@ let initialize = function () {
   ctx = canvas.getContext("2d");
   window.addEventListener('resize', resizeCanvas, false);
   resizeCanvas();
-  window.addEventListener('keydown', function (e) {
-    keysDown[e.keyCode] = true;
-  });
-  window.addEventListener('keyup', function (e) {
-    delete keysDown[e.keyCode];
-  });
   reset();
   gameLoop();
 };
 
 let reset = function () {
-  hero.x = 0;
-  hero.y = 0;
+  hero.reset();
   lastUpdate = Date.now();
 };
 
@@ -35,53 +27,7 @@ let gameLoop = function () {
 };
 
 let tick = function (delta: number) {
-  // Limit velocity positive x
-  if (hero.xVelocity > hero.xVelocityMax) { hero.xVelocity = hero.xVelocityMax; }
-  // Limit velocity negative x
-  if (hero.xVelocity < -hero.xVelocityMax) { hero.xVelocity = -hero.xVelocityMax; }
-
-  // Only accept input if on ground
-  if (hero.y <= 0) {
-    // Jump
-    if (GameKeys.up in keysDown) { hero.yVelocity += hero.jumpStrenght; }
-  }
-  if (GameKeys.left in keysDown) {
-    hero.xVelocity -= hero.xVelocityIncrease;
-  }
-  else if (GameKeys.right in keysDown) {
-    hero.xVelocity += hero.xVelocityIncrease;
-  }
-  // Not pressing anything
-  else {
-    // If going positive x, decrease velocity gradually
-    if (hero.xVelocity >= 0) {
-      hero.xVelocity -= hero.xVelocityDecrease;
-    }
-    // If going negative x, increase velocity gradually
-    if (hero.xVelocity < 0) {
-      hero.xVelocity += hero.xVelocityDecrease;
-    }
-  }
-
-  hero.x += hero.xVelocity * delta // Apply x velocity
-  hero.yVelocity -= hero.gravity; // Apply gravity
-  hero.y += hero.yVelocity * delta // Apply y velocity
-
-  // If hero hits left wall, stop
-  if (hero.x < 0) {
-    hero.x = 0;
-    hero.xVelocity = 0;
-  }
-  // If hero hits ground, stop
-  if (hero.y < 0) {
-    hero.y = 0;
-    hero.yVelocity = 0;
-  }
-  // If hero hits ceiling, stop
-  if (hero.y > ctx.canvas.height) {
-    hero.y = ctx.canvas.height;
-    hero.yVelocity = 0;
-  }
+  hero.tick(delta);
   draw();
 };
 
@@ -89,14 +35,7 @@ let draw = function () {
   // Clear canvas
   ctx.fillStyle = "#000000";
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-  // Draw hero
-  if (hero.imageReady) {
-    ctx.drawImage(hero.image,
-      hero.x,
-      ctx.canvas.height - hero.y - hero.hitboxHeight
-    );
-  }
+  hero.draw(ctx);
 };
 
 let resizeCanvas = function () {
--- ts/objects.ts
+++ ts/objects.ts
@@ -1,12 +1,16 @@
 class Sprite {
-    x = 0;
-    y = 0;
+    xInitial = 0;
+    yInitial = 0;
+    x: number;
+    y: number;
     hitboxWidth = 0;
     hitboxHeight = 0;
     image = document.createElement("img");
     imageReady = false;
 
     constructor(imageSource: string) {
+        this.x = this.xInitial;
+        this.y = this.yInitial;
         this.image.src = imageSource;
         var self = this;
         this.image.onload = (e) => {
@@ -18,16 +22,98 @@ class Sprite {
         this.hitboxHeight = this.image.height;
         this.hitboxWidth = this.image.width;
     }
+    reset() {
+        this.x = this.xInitial;
+        this.y = this.yInitial;
+    }
+    draw(canvasRenderingContext: CanvasRenderingContext2D) {
+        if (this.imageReady) {
+            canvasRenderingContext.drawImage(
+                this.image,
+                this.x,
+                canvasRenderingContext.canvas.height - this.y - this.hitboxHeight
+            );
+        }
+    }
 }
 
 class LivingSprite extends Sprite {
     xVelocity = 0;
     yVelocity = 0;
-    xVelocityMax = 255;
+    xVelocityMax = 250;
     xVelocityIncrease = 30;
     xVelocityDecrease = 15;
     gravity = 15;
     jumpStrenght = 500;
+    reset() {
+        this.x = this.xInitial;
+        this.y = this.yInitial;
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+    }
+}
+
+class PlayerSprite extends LivingSprite {
+    keysDown: boolean[] = new Array<boolean>();
+    constructor(imageSource: string) {
+        super(imageSource);
+        var self = this;
+        window.addEventListener('keydown', (e) => {
+            self.keysDown[e.keyCode] = true;
+        });
+        window.addEventListener('keyup', (e) => {
+            delete self.keysDown[e.keyCode];
+        });
+    }
+    tick(delta: number) {
+        // Limit velocity positive x
+        if (this.xVelocity > this.xVelocityMax) { this.xVelocity = this.xVelocityMax; }
+        // Limit velocity negative x
+        if (this.xVelocity < -this.xVelocityMax) { this.xVelocity = -this.xVelocityMax; }
+
+        // Only accept input if on ground
+        if (this.y <= 0) {
+            // Jump
+            if (GameKeys.up in this.keysDown) { this.yVelocity += this.jumpStrenght; }
+        }
+        if (GameKeys.left in this.keysDown) {
+            this.xVelocity -= this.xVelocityIncrease;
+        }
+        else if (GameKeys.right in this.keysDown) {
+            this.xVelocity += this.xVelocityIncrease;
+        }
+        // Not pressing anything
+        else {
+            // If going positive x, decrease velocity gradually
+            if (this.xVelocity >= 0) {
+                this.xVelocity -= this.xVelocityDecrease;
+            }
+            // If going negative x, increase velocity gradually
+            if (this.xVelocity < 0) {
+                this.xVelocity += this.xVelocityDecrease;
+            }
+        }
+
+        this.x += this.xVelocity * delta // Apply x velocity
+        this.yVelocity -= this.gravity; // Apply gravity
+        this.y += this.yVelocity * delta // Apply y velocity
+
+        // If this hits left wall, stop
+        if (this.x < 0) {
+            this.x = 0;
+            this.xVelocity = 0;
+        }
+        // If this hits ground, stop
+        if (this.y < 0) {
+            this.y = 0;
+            this.yVelocity = 0;
+        }
+        // If this hits ceiling, stop
+        if (this.y > ctx.canvas.height) {
+            this.y = ctx.canvas.height;
+            this.yVelocity = 0;
+        }
+    }
 }
 
 enum GameKeys {
