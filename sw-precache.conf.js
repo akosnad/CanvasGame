b227aed77b2c15a24eb4659daf76efddd2744e7e
akosnad <akos.nadudvari@pinkcloud.hu>
2018-08-18 15:30:14 +0200

    PWA capabilities added

---
+++ sw-precache.conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+    navigateFallback: '/index.html',
+    stripPrefix: 'wwwroot',
+    root: 'wwwroot/',
+    staticFileGlobs: [
+      'wwwroot/index.html',
+      'wwwroot/**.css',
+      'wwwroot/js/**',
+      'wwwroot/lib/**'
+    ]
+  };
+  
