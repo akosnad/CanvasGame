7d092ad97731e36cd05c4404d2b4d886ba382add
akosnad <akos.nadudvari@pinkcloud.hu>
2018-08-20 15:50:36 +0200

    Npm audit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 
-gulp.task('default', ['build-pug', 'copy-dist']);
 
 gulp.task('watch', function () {
     gulp.watch('./pug/**', ['build-pug']);
@@ -9,29 +8,32 @@ gulp.task('watch', function () {
 
 gulp.task('build-pug', function() {
     return gulp.src('./pug/*.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('./wwwroot/'));
+    .pipe(pug())
+    .pipe(gulp.dest('./wwwroot/'));
 });
 
-gulp.task('copy-dist', function () {
+gulp.task('copy-dist', function (done) {
     gulp.src('./node_modules/jquery/dist/jquery.min.js')
-        .pipe(gulp.dest('./wwwroot/lib/js/'));
+    .pipe(gulp.dest('./wwwroot/lib/js/'));
     gulp.src('./node_modules/mdbootstrap/js/popper.min.js')
-        .pipe(gulp.dest('./wwwroot/lib/js/'));
+    .pipe(gulp.dest('./wwwroot/lib/js/'));
     gulp.src('./node_modules/mdbootstrap/js/bootstrap.min.js')
-        .pipe(gulp.dest('./wwwroot/lib/js'));
+    .pipe(gulp.dest('./wwwroot/lib/js'));
     gulp.src('./node_modules/mdbootstrap/js/mdb.min.js')
-        .pipe(gulp.dest('./wwwroot/lib/js'));
+    .pipe(gulp.dest('./wwwroot/lib/js'));
     gulp.src('./node_modules/@aspnet/signalr/dist/browser/signalr.min.js')
-        .pipe(gulp.dest('./wwwroot/lib/js'));
+    .pipe(gulp.dest('./wwwroot/lib/js'));
     gulp.src('./node_modules/file-saver/FileSaver.min.js')
-        .pipe(gulp.dest('./wwwroot/lib/js'));
+    .pipe(gulp.dest('./wwwroot/lib/js'));
     
-
+    
     gulp.src('./node_modules/mdbootstrap/css/bootstrap.min.css')
-        .pipe(gulp.dest('./wwwroot/lib/css'));
+    .pipe(gulp.dest('./wwwroot/lib/css'));
     gulp.src('./node_modules/mdbootstrap/css/mdb.min.css')
-        .pipe(gulp.dest('./wwwroot/lib/css'));
+    .pipe(gulp.dest('./wwwroot/lib/css'));
     gulp.src('./node_modules/mdbootstrap/font/roboto/*')
-        .pipe(gulp.dest('./wwwroot/lib/font/roboto'));
+    .pipe(gulp.dest('./wwwroot/lib/font/roboto'));
+    done();
 });
+
+gulp.task('default', gulp.parallel(['build-pug', 'copy-dist']));
